9699d76e25412a301d37bba5d9f0bbb3dd64fa36
alireza <lyrdaaymany37@gmail.com>
2025-09-17 15:14:06 +0330

    fix

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,5 +2,5 @@ import app from "./httpService";
 import type { GetOtpRequest } from "../types/auth";
 
 export function getOtp(data: GetOtpRequest) {
-  return app.post("/user/get-otp", data).then(({ data }) => data.data)
+  return app.post("/user/get-otp", data).then(({ data }) => data.data);
 }
