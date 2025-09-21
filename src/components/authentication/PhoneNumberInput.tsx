83dc95111c84d1131e92a72dbc0f2b5032a34d49
alireza <lyrdaaymany37@gmail.com>
2025-09-21 20:34:08 +0330

    refactor(auth): redesign login form, fix bugs, update i18n keys, and split into smaller components

--- src/components/authentication/PhoneNumberInput.tsx
+++ src/components/authentication/PhoneNumberInput.tsx
@@ -7,7 +7,13 @@ interface PhoneNumberInputProps {
 const PhoneNumberInput = ({ placeholder, value, onChange }: PhoneNumberInputProps) => {
   return (
     <>
-      <input type="number" value={value} onChange={onChange} placeholder={placeholder} className="w-full h-10 px-2 py-1 border-black border-2 rounded-sm outline-none" />
+      <input
+        type="number"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full h-10 px-2 py-1 border-black border-2 rounded-sm outline-none"
+      />
     </>
   );
 };
