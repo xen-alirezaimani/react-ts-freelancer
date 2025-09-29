fa3ea24a9322e6877982306aa805f7f1324009b5
alireza <lyrdaaymany37@gmail.com>
2025-09-29 22:37:13 +0330

    edit auth page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -2,7 +2,7 @@ import SendOTP from "../features/authentication/SendOTP";
 
 export default function AuthPage() {
   return (
-    <div className="container min-h-screen">
+    <div className="container h-[100dvh] overflow-hidden bg-red-500">
       <div className="fixed top-1/2 left-1/2 h-100 max-h-[calc(100vh-2rem)] w-[calc(100vw-2rem)] -translate-1/2 overflow-y-auto rounded-2xl border p-4 md:max-w-sm">
         <SendOTP />
       </div>
