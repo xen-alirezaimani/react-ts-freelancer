632722a9dbf0fb14e3453b7a91fb8c842cc7619a
alireza <lyrdaaymany37@gmail.com>
2025-11-08 21:17:13 +0330

    refactor: Edit TextInput and CompleteProfile for show  real-time errors

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,19 +1,20 @@
-import type { FieldErrors } from "react-hook-form";
+import type { FieldErrors, FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-interface PropsType {
-  type: "email" | "text";
+interface PropsType<T extends FieldValues> {
+  type: "email" | "password" | "text";
   placeholder: string;
-  name: string;
-  errors?: FieldErrors;
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  errors?: FieldErrors<T>;
 }
 
-const TextInput = ({ type, placeholder, name, errors }: PropsType) => {
+const TextInput = <T extends FieldValues>({ type, placeholder, name, register, errors }: PropsType<T>) => {
   const errorMessage = errors?.[name]?.message as string | undefined;
   console.log(errorMessage);
 
   return (
     <div>
-      <input type={type} placeholder={placeholder} {...(name && { name })} />
+      <input type={type} placeholder={placeholder} {...register(name)} />
       {errorMessage && <span>{errorMessage}</span>}
     </div>
   );
