8fce13f46c1ef75a07c91f040f23caa947e0ca4c
alireza <lyrdaaymany37@gmail.com>
2025-11-09 20:12:23 +0330

    refactor-fix: Refactor and fix TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,7 @@
 import type { FieldErrors, FieldValues, Path, UseFormRegister } from "react-hook-form";
 
+import { useState } from "react";
+
 interface PropsType<T extends FieldValues> {
   type: "email" | "password" | "text";
   placeholder: string;
@@ -8,14 +10,47 @@ interface PropsType<T extends FieldValues> {
   errors?: FieldErrors<T>;
 }
 
+const isPersian = (text: string): boolean => {
+  const persianRegex = /[\u0600-\u06ff]/;
+  return persianRegex.test(text);
+};
+
 const TextInput = <T extends FieldValues>({ type, placeholder, name, register, errors }: PropsType<T>) => {
   const errorMessage = errors?.[name]?.message as string | undefined;
-  console.log(errorMessage);
+  const [direction, setDirection] = useState<"auto" | "ltr" | "rtl">(type === "email" || type === "password" ? "ltr" : "auto");
+  // const inputDirection = type === "email" || type === "password" ? "ltr" : "auto";
+
+  // مدیریت تغییر جهت بر اساس ورودی
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (type === "text") {
+      const value = e.target.value;
+      if (value.length > 0) {
+        setDirection(isPersian(value[0]) ? "rtl" : "ltr");
+      } else {
+        setDirection("auto");
+      }
+    }
+  };
+
+  const placeholderClass =
+    type === "email" || type === "password"
+      ? "text-left rtl:placeholder:text-right" // همیشه چپ برای email و password
+      : direction === "rtl" || direction === "auto"
+        ? "rtl:placeholder:text-right ltr:placeholder:text-left" // وابسته به جهت صفحه برای text
+        : "";
 
   return (
-    <div>
-      <input type={type} placeholder={placeholder} {...register(name)} />
-      {errorMessage && <span>{errorMessage}</span>}
+    <div className="relative w-full">
+      <input
+        className={`w-full rounded-sm border p-2 ${direction === "rtl" ? "text-right" : "text-left"} ${placeholderClass}`}
+        type={type}
+        placeholder={placeholder}
+        {...register(name)}
+        onInput={handleInputChange}
+      />
+      <div className="absolute">
+        <p className="text-sm text-red-500">{errorMessage}</p>
+      </div>
     </div>
   );
 };
